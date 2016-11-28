917ee7affbd6b91ab4db1cafe99a180e54a30c17
zhoujian <1064791247@qq.com>
2016-11-28 14:49:50 +0800

    zeke   提交代码

--- app/src/main/java/com/zhoujian/android/activity/FourthActivity.java
+++ app/src/main/java/com/zhoujian/android/activity/FourthActivity.java
@@ -1,9 +1,15 @@
 package com.zhoujian.android.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.zhoujian.android.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by zhoujian on 2016/11/28.
@@ -12,13 +18,32 @@ import com.zhoujian.android.R;
 public class FourthActivity extends Activity {
 
 
+    @InjectView(R.id.bt_start)
+    Button mBtStart;
+
     //option+command+k   对生命
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fourth);
+        ButterKnife.inject(this);
+        clickEvent();
 
+
+
+
+    }
+
+    private void clickEvent()
+    {
+        mBtStart.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FourthActivity.this,FifthActivity.class));
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/zhoujian/android/activity/ThirdActivity.java
+++ app/src/main/java/com/zhoujian/android/activity/ThirdActivity.java
@@ -1,9 +1,12 @@
 package com.zhoujian.android.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.zhoujian.android.R;
@@ -15,14 +18,18 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Created by zhoujian on 2016/11/16.
  */
 
-public class ThirdActivity  extends Activity
-{
+public class ThirdActivity extends Activity {
 
     public static final String TAG = "ThirdActivity";
+    @InjectView(R.id.bt_start)
+    Button mBtStart;
 
     private File file;
 
@@ -30,65 +37,72 @@ public class ThirdActivity  extends Activity
 
     //option+command+k  对生命周期方法进行排序
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
-        try
-        {
+        ButterKnife.inject(this);
+
+
+        clickEvent();
+
+
+        try {
             File extDir = Environment.getExternalStorageDirectory();
             String filename = "catch.txt";
             fullFilename = new File(extDir, filename);
             fullFilename.createNewFile();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         //序列化过程
-        try
-        {
-            if(sdCardExit())
-            {
-                Person person = new Person(true,0,"zhoujian");
+        try {
+            if (sdCardExit()) {
+                Person person = new Person(true, 0, "zhoujian");
                 ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fullFilename.getAbsoluteFile()));
                 out.writeObject(person);
                 out.close();
             }
-        }
-        catch (Exception  e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         //反序列化过程
-        try
-        {
-            if(sdCardExit()){
+        try {
+            if (sdCardExit()) {
                 ObjectInputStream in = new ObjectInputStream(new FileInputStream(fullFilename.getAbsoluteFile()));
-                Person newPerson  =(Person) in.readObject();
-                Log.e(TAG,"反序列化成功-----newPerson==="+newPerson.toString());
-                Toast.makeText(this,  "反序列化成功-----newPerson===" +newPerson.toString(), Toast.LENGTH_SHORT).show();
+                Person newPerson = (Person) in.readObject();
+                Log.e(TAG, "反序列化成功-----newPerson===" + newPerson.toString());
+                Toast.makeText(this, "反序列化成功-----newPerson===" + newPerson.toString(), Toast.LENGTH_SHORT).show();
                 in.close();
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+
+    public void clickEvent()
+    {
+        mBtStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ThirdActivity.this,FourthActivity.class));
+            }
+        });
+
+    }
+
     /**
      * 判断SD卡是否可用
+     *
      * @return
      */
-    private static boolean sdCardExit(){
+    private static boolean sdCardExit() {
 
         return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
     }
-
 
 
     @Override
