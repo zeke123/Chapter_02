6548e8d19330e43ac31f712718929a7365fd0940
zhoujian <1064791247@qq.com>
2016-11-28 17:23:33 +0800

    zeke   提交代码

--- app/src/main/java/com/zhoujian/android/activity/MessagerActivity.java
+++ app/src/main/java/com/zhoujian/android/activity/MessagerActivity.java
@@ -6,9 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.util.Log;
 import com.zhoujian.android.R;
 import com.zhoujian.android.service.MessagerService;
 
@@ -19,7 +21,32 @@ import com.zhoujian.android.service.MessagerService;
 public class MessagerActivity extends Activity
 {
 
+
+    public static final String TAG="MessagerActivity";
+
     public static final int MSG_FROM_CLIENT=1;
+
+
+    //创建接受服务端消息的Messenger和Handler
+
+    Handler handler = new Handler()
+    {
+        private String mReply;
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what)
+            {
+                case 2:
+                    mReply = msg.getData().getString("reply");
+                    Log.e(TAG,"接受的消息来自服务端："+mReply);
+                    break;
+            }
+        }
+    };
+
+
+
+    Messenger  replyMessenger = new Messenger(handler);
 
     ServiceConnection conn = new ServiceConnection()
     {
@@ -44,6 +71,8 @@ public class MessagerActivity extends Activity
 
             //设置数据
             msg.setData(bundle);
+
+            msg.replyTo = replyMessenger;
 
             try
             {
--- app/src/main/java/com/zhoujian/android/service/MessagerService.java
+++ app/src/main/java/com/zhoujian/android/service/MessagerService.java
@@ -2,6 +2,7 @@ package com.zhoujian.android.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -23,6 +24,7 @@ public class MessagerService extends Service
     Handler handler = new Handler()
     {
 
+        private Messenger mTo;
         private String  messager;
 
         @Override
@@ -34,6 +36,29 @@ public class MessagerService extends Service
 
                    messager = msg.getData().getString("messager");
                    Log.e(TAG,"接受的消息来自客户端："+messager);
+
+                   //服务端收到客户端的消息后，在发送一条消息到客户端
+
+                   mTo = msg.replyTo;
+
+                   //创建Message
+
+                   Message mMessae = Message.obtain(null,2);
+
+                   Bundle bundle = new Bundle();
+
+                   bundle.putString("reply","您的消息已经收到，稍后给您回复！");
+
+                   mMessae.setData(bundle);
+
+                   try
+                   {
+                       mTo.send(mMessae);
+                   }
+                   catch (Exception e)
+                   {
+                       e.printStackTrace();
+                   }
            }
        }
     };
